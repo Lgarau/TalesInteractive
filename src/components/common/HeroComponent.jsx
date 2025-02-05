313c10ca779c67256435f2969e159ea7b4b1ee84
CarloDettori <dettoric07@gmail.com>
2025-02-05 20:40:36 +0100

    fixed video button changed to <Link> and linked to project page

--- src/components/common/HeroComponent.jsx
+++ src/components/common/HeroComponent.jsx
@@ -1,23 +1,26 @@
+import { Link } from "react-router-dom";
+
 export default function HeroComponent() {
   return (
-    <div className="relative flex flex-col items-center justify-center text-center bg-black py-32">
-
+    <div className="relative flex flex-col items-center justify-center text-center bg-black py-32 text-white">
 
       {/* Video background */}
-      <div className="absolute inset-0 z-0">
+      <div className="absolute inset-0 z-0 bg-black">
         <video
-          className="w-full h-full object-cover"
+          className="w-full h-full object-cover bg-gray-950"
         >
-          <source src="" type="video/mp4" />
+          <source className="" src="https://www.youtube.com/watch?v=MLpWrANjFbI" type="video/mp4" />
 
         </video>
       </div>
 
-      <h1 className="text-5xl font-bold text[#f5f5dc] z-10">Video qui</h1>
-      <div>
-        <button className="mt-50 px-6 py-3 bg-[#a0522d] text-[#f5f5dc] rounded-lg shadow-lg hover:bg-opacity-80 z-10">
-          scopri di più
-        </button>
+      <h1 className="mb-40 text-5xl font-bold text[#f5f5dc] z-100">Video qui</h1>
+      <div className="z-1000">
+        <Link
+          to="/progetti"
+          className="mt-50 px-6 py-3 bg-[#a0522d] text-[#f5f5dc] rounded-lg shadow-lg hover:bg-opacity-80 ">
+          Scopri di più
+        </Link>
       </div>
     </div>
   );
--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -1,11 +1,13 @@
 import Hero from "../common/HeroComponent";
 import ProjectsPage from "./ProjectsPage";
 import TeamPage from "./TeamPage"
+import HeroComponent from "../common/HeroComponent";
 
 export default function HomePage() {
   return (
     <div className="min-h-screen bg-black text-white">
-      <h1>HomePage</h1>
+      <HeroComponent />
+
     </div>
   );
 }
