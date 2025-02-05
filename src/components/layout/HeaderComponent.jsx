148887fa929a8295ee4330b97a23bfa16b96529b
CarloDettori <dettoric07@gmail.com>
2025-02-05 12:47:04 +0100

    aggiustato header secondo le richieste di daniele

--- src/components/layout/HeaderComponent.jsx
+++ src/components/layout/HeaderComponent.jsx
@@ -1,22 +1,21 @@
 import { NavLink } from "react-router-dom";
-import Logo from "../../assets/Logow.png";
+import Logo from "../../assets/bianco-logo.png";
 
 export default function HeaderComponent() {
 
     return (
         <header>
 
-            <div>
-                <img src={Logo} alt="TalesInteractiveLogo" className="h-40 text-white" />
-            </div>
+            <nav className="flex justify-between items-center px-40 py-5 border-b border-gray-700 space-x-8 text-[#a0522d] font-semibold">
 
-            <nav className="flex justify-between items-center px-40 py-5 border-b border-gray-700">
-                <div className="space-x-8 text-[#a0522d] font-semibold">
-                    <NavLink to="/">Home</NavLink>
-                    <NavLink to="/team">Progetti</NavLink>
-                    <NavLink to="/progetti">Team</NavLink>
-                    <NavLink to="/contatti">Contattaci</NavLink>
-                </div>
+                <NavLink to="/">Home</NavLink>
+                <NavLink to="/team">Progetti</NavLink>
+                <NavLink to="/"> <img src={Logo} alt="TalesInteractiveLogo" id="logo" className="h-10 text-white" /> </NavLink>
+
+
+                <NavLink to="/progetti">Team</NavLink>
+                <NavLink to="/contatti">Contattaci</NavLink>
+
             </nav>
 
         </header>
