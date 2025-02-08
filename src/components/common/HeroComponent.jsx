6f623483258af44b3a39bbeba1bd23978e2a3436
CarloDettori <dettoric07@gmail.com>
2025-02-08 13:32:09 +0100

    fixing footer + here button centered but still need pisitioning in responsive

--- src/components/common/HeroComponent.jsx
+++ src/components/common/HeroComponent.jsx
@@ -7,19 +7,19 @@ export default function HeroComponent() {
 
 
   return (
-    <div className="h-150 bg-none relative  flex items-center justify-center text-center relative text-white">
+    <div className="h-150 bg-none relative flex items-center justify-center text-center relative text-white">
 
 
       <img src={imgPH} alt="Game-Frame" className="h-full z-900 w-full bg-none object-contain" />
-      <div id="links" className="absolute mt-50 translate z-1000" >
+      <div id="links" className="translate z-1000" >
         <Link
           to="/progetti"
-          className="bottom-10 absolute ml-50 w-40 px-6 py-3 bg-[#a0522d] text-[#f5f5dc] rounded-lg shadow-lg hover:bg-opacity-80 z-1000">
+          className="absolute ml-50 w-40 px-6 py-3 bg-[#a0522d] text-[#f5f5dc] rounded-lg shadow-lg hover:bg-opacity-80 z-1000">
           Vedi Trailer
         </Link>
         <Link
           to="/progetti"
-          className="bottom-10 absolute mr-50 w-40 px-6 py-3 bg-[#a0522d] text-[#f5f5dc] rounded-lg shadow-lg hover:bg-opacity-80 z-1000">
+          className="absolute mr-50 w-40 px-6 py-3 bg-[#a0522d] text-[#f5f5dc] rounded-lg shadow-lg hover:bg-opacity-80 z-1000">
           Scopri di più
         </Link>
       </div>
--- src/components/layout/FooterComponent.jsx
+++ src/components/layout/FooterComponent.jsx
@@ -3,7 +3,7 @@ export default function FooterComponent() {
     return (
         <footer className="flex p-10 place-content-around item-center bg-black text-white text-center">
             <img className=" max-w-50" src={talInt} alt="" />
-            <h1 className="inline-block  text-white">  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempora ad eligendi dolorum, ratione corrupti sapiente!  </h1>
+            <h1 className="inline-block my-auto text-white">  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempora ad eligendi dolorum, ratione corrupti sapiente!  </h1>
         </footer>
     );
 }
