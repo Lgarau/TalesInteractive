900ba710af948881ed6d93ea086ad9ff23d85da0
CarloDettori <dettoric07@gmail.com>
2025-02-09 14:29:05 +0100

    h-auto for team member carousel

--- src/components/unique/TeamComponent.jsx
+++ src/components/unique/TeamComponent.jsx
@@ -4,8 +4,8 @@ import TeamListComponent from "../common/TeamListComponent";
 export default function TeamComponent({ }) {
 
     return (
-        <div className="overflow-x-scroll p-0 m-0" >
-            <div className="flex gap-12 w-1600 my-30 h-150 flex-nowrap text-center">
+        <div className="overflow-x-scroll" >
+            <div className="flex gap-12 w-1600 my-30 h-auto flex-nowrap text-center">
 
                 {teamMembers.map((member) => {
                     return <TeamListComponent key={member.id} name={member.name} surname={member.surname} role={member.role} image={member.img} />
