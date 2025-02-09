3242cff14b9b8d85f2b82a4673060d39cc8d81dc
CarloDettori <dettoric07@gmail.com>
2025-02-09 01:23:03 +0100

    trying for a better overflow layout in CarouselComponent.jsx TeamComponent.jsx, need fixing

--- src/components/common/CarouselComponent.jsx
+++ src/components/common/CarouselComponent.jsx
@@ -5,12 +5,9 @@ export default function CarouselComponent({ }) {
 
     return (
         <div className="flex overflow-x-scroll">
-
             {carouselPost.map((post) => {
                 return <CarouselContentComponent key={post.id} title={post.title} content={post.content} image={post.img} />
             })}
-
-
         </div>
     );
 }
--- src/components/unique/CarouselContentComponent.jsx
+++ src/components/unique/CarouselContentComponent.jsx
@@ -2,7 +2,7 @@ export default function CarouselContent({ title, content, image }) {
 
     return (
         <div className="p-8 w-500 shadow-md text-center">
-            <img src={image} className=" fa-solid fa-user-large h-100 rounded-sm mx-auto mb-4 " alt="carousel-post-img" />
+            <img src={image} className="carousel-img fa-solid fa-user-large h-100 rounded-sm mx-auto mb-4 " alt="carousel-post-img" />
             <h2 className="text-4xl font-bold text-center mb-6">{title}</h2>
             <p className="text-lg text-white-700">
                 {content}
