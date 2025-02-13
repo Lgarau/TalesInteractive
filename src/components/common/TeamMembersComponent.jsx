4140b25179dd450e0619e358a5c6384930e80dd6
CarloDettori <dettoric07@gmail.com>
2025-02-13 01:12:58 +0100

    vertical team member cpmponent + dercription and image is alternated in member card

--- src/components/common/TeamMembersComponent.jsx
+++ src/components/common/TeamMembersComponent.jsx
@@ -5,10 +5,10 @@ export default function TeamMembersComponent({ }) {
 
     return (
         <div className="overflow-x-scroll" >
-            <div className="flex gap-12 w-1600 my-30 h-auto flex-nowrap text-center">
+            <div className="member-row gap-12 my-30 h-auto flex-nowrap text-center px-">
 
                 {teamMembers.map((member) => {
-                    return <TeamList key={member.id} name={member.name} surname={member.surname} role={member.role} image={member.img} />
+                    return <TeamList key={member.id} n={member.id} name={member.name} surname={member.surname} role={member.role} image={member.img} />
 
                 })}
             </div>
--- src/components/unique/TeamList.jsx
+++ src/components/unique/TeamList.jsx
@@ -1,12 +1,28 @@
 
 
-export default function TeamListComponent({ name, surname, role, image }) {
-
-    return (
-        <div className="p-8">
-            <img className="w-100 rounded-full" src={image} alt="" />
+export default function TeamListComponent({ name, n, surname, role, image }) {
+    let picture = <img className="w-100 p-5 rounded-full" src={image} alt="" />
+    let description =
+        <div className="flex flex-col p-5 justify-center">
             <h3 className="block my-4 text-4xl font-semibold text-[#a0522d] mb-2">{name} {surname}</h3>
             <p className="text-3xl my-3 text-gray-700">{role}</p>
         </div>
+
+    let template
+    if (n % 2 == 0) {
+        template =
+            <div className="flex p-8 justify-center">
+                {description}
+                {picture}
+            </div>
+    } else {
+        template =
+            <div className="flex p-8 justify-center" >
+                {picture}
+                {description}
+            </div>
+    }
+    return (
+        template
     );
 }
