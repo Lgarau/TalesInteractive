d0b60284672770595442c852a11521f0b9a3ba00
CarloDettori <dettoric07@gmail.com>
2025-02-21 18:59:59 +0100

    removing buttons from hero and adding hero on projects

--- src/components/common/HeroComponent.jsx
+++ src/components/common/HeroComponent.jsx
@@ -17,12 +17,12 @@ export default function HeroComponent() {
       <p className="absolute mb-60 w-100 px-6 py-3 z-1000 text-black">Sassari, 1349. <br /> Una giovane donna... </p>
       <Link
         to="/progetti"
-        className="absolute mt-90 ml-50 w-40 px-6 py-3 bg-[#a0522d] text-[#f5f5dc] rounded-full shadow-lg hover:bg-opacity-80 z-1000">
+        className="hidden absolute mt-90 ml-50 w-40 px-6 py-3 bg-[#a0522d] text-[#f5f5dc] rounded-full shadow-lg hover:bg-opacity-80 z-1000">
         Vedi Trailer
       </Link>
       <Link
         to="/progetti"
-        className="absolute mt-90 mr-50 w-40 px-6 py-3 bg-[#a0522d] text-[#f5f5dc] rounded-full shadow-lg hover:bg-opacity-80 z-1000">
+        className="absolute mt-90 mr-0 w-40 px-6 py-3 bg-[#a0522d] text-[#f5f5dc] rounded-full shadow-lg hover:bg-opacity-80 z-1000">
         Scopri di più
       </Link>
     </div>
--- src/components/pages/ProjectsPage.jsx
+++ src/components/pages/ProjectsPage.jsx
@@ -1,11 +1,12 @@
+import HeroComponent from "../common/HeroComponent";
+
 export default function ProjectsPage() {
   return (
 
 
-    <section>
-      <h2 className="text-4xl font-semibold text-center mb-12 text-[#a0522d]">I Nostri Progetti</h2>
-      <p className="text-center">tante belle cose</p>
-    </section>
+
+    <HeroComponent />
+
 
 
   );
