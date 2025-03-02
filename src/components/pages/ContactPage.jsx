e66a17943e86f7fc881106a0843860ce92dd6f1c
CarloDettori <dettoric07@gmail.com>
2025-03-02 13:41:55 +0100

    sostituito testo per l'informativa sulla privacy

--- src/components/pages/ContactPage.jsx
+++ src/components/pages/ContactPage.jsx
@@ -144,7 +144,7 @@ export default function ContactPage() {
         </div>
         <div className="flex items-center">
           <input required id="link-checkbox" type="checkbox" value="" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
-          <label htmlFor="link-checkbox" className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">I agree with the <button onClick={toggleTerms} href="#" className="text-blue-600 dark:text-blue-500 hover:underline">terms and conditions</button>.</label>
+          <label htmlFor="link-checkbox" className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Accetto l' <button onClick={toggleTerms} href="#" className="text-blue-600 dark:text-blue-500 hover:underline">informativa sulla privacy</button>.</label>
         </div>
         <button
           type="submit"
