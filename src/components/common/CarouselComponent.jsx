7c71dfef0badefa59d3e4ec9fe3b3305f48a2507
CarloDettori <dettoric07@gmail.com>
2025-03-02 16:46:00 +0100

    some carousel fix

--- src/components/common/CarouselComponent.jsx
+++ src/components/common/CarouselComponent.jsx
@@ -5,12 +5,10 @@ export default function CarouselComponent({ }) {
     function CarouselContent({ title, content, image }) {
 
         return (
-            <div className="carousel-card p-8 w-500 shadow-md text-center">
+            <div className="carousel-card p-8 shadow-md text-center">
                 <img src={image} className="carousel-img fa-solid fa-user-large h-auto rounded-md mx-auto mb-4 " alt="carousel-post-img" />
                 <p className="text-3xl font-bold text-center my-6">{title}</p>
-                <p className="text-lg text-white-700">
-                    {content}
-                </p>
+                <p className="text-lg text-white-700">{content}</p>
             </div>
         );
     }
